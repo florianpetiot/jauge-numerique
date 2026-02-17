d0eedb9894b657952e844c0a2468e2d6020b3c91
E228212G <esteban.giroux@etu.univ-nantes.fr>
2026-02-17 15:56:34 +0100

    Ajout Dossier data contenant normes & services de recherche correspondance & utils de conversion mm vers TPI

---
+++ backend/services/find_match.py
@@ -0,0 +1,29 @@
+import json
+from utils.metric_conversion import TPI_to_pas, pas_to_TPI
+
+def find_match(mesured_diam_mm, tolerance_diam, mesured_pas, tolerance_pas, dimension_filepath):
+    with open(dimension_filepath, 'r') as file:
+        dimensions = json.load(file)
+
+    acceptable_diams = []
+    for dimension in dimensions.keys():
+        if abs(dimensions[dimension]['diam_mm'] - mesured_diam_mm) <= tolerance_diam:
+            acceptable_diams.append(dimension)
+
+    acceptables = []
+    for dimension in acceptable_diams:
+        pas = -1
+        if dimensions[dimension]['unite'] == 'I':
+            pas = TPI_to_pas(dimensions[dimension]['pas'])
+        else: 
+            pas = dimensions[dimension]['pas']
+
+        if abs(pas - mesured_pas) <= tolerance_pas:
+            acceptables.append(dimension)
+
+    for acceptable in acceptables:
+        print(acceptable, dimensions[acceptable])
+
+    return acceptables
+
+# find_match(13.2,0.1,1.24,0.1,"data/Dimensions.json")
---
+++ backend/utils/metric_conversion.py
@@ -0,0 +1,5 @@
+def pas_to_TPI(pas):
+    return 25.4/pas
+
+def TPI_to_pas(TPI):
+    return 25.4/TPI
